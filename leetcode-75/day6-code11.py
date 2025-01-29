40647b9a5a43ecaf49cf5fd2d356ec1d98dd5006
MuzzammilShah <muhammed.muzzammilshah@gmail.com>
2025-01-29 07:01:52 +0530

    Day 6 Leetcode-2

--- leetcode-75/day6-code11.py
+++ leetcode-75/day6-code11.py
@@ -21,3 +21,44 @@
 # s and t consist only of lowercase English letters.
 
 #=====SOLUTION=====
+
+def is_subsequence(s, t):
+
+    s_ptr, t_ptr = 0, 0
+
+    if len(s) == 0:
+        return True
+
+    while t_ptr < len(t):
+
+        if t[t_ptr] == s[s_ptr]:
+
+            s_ptr += 1
+        
+        t_ptr += 1
+
+    if s_ptr == len(s):
+        return True
+    else:
+        return False
+    
+
+def main():
+
+    s_input = input("Enter small string: ")
+    t_input = input("Enter larger string: ")
+    result= is_subsequence(s_input, t_input)
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
+
+#=====Explaination=====
+
+# t should keep traversing
+# s should only increament if there is a match
+# if the total increament value of s pointer is same as the lenght of s, then it is true
+
+# for intial checks:
+# if s is zero then it should be true
+# if t is 0 and s is not zero, then it should return false
